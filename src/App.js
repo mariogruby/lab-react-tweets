ec2f650c997d5127b9fa5c809b979b2ba3d677fe
mario ruby <mariogabrielrn@gmail.com>
2023-02-27 17:34:59 +0100

    done

--- src/App.js
+++ src/App.js
@@ -37,7 +37,11 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      
+      {tweetsArray.map((tweet, index)=>(
+        <Tweet key={index} tweet={tweet}/>
+
+      ))}
     </div>
   );
 }
